7a2a26ca21239b4a1c9c4ad026583081eba719f2
ABRAM <ABRAM@ABRAM-Deng.SouthHuitong.csr.com>
2017-10-07 23:27:20 +0800

    提交Map示例和注释

--- Java/src/container/TestMap.java
+++ Java/src/container/TestMap.java
@@ -5,13 +5,15 @@ import java.util.*;
  * @author ABRAM 2017.10.07
  *	map的应用举例，使用put方法放入键值对，containsValue判断是否包含值，containsKey判断是否包含键，
  *  对于treemap和hashmap的区别没有继续研究
+ *  Auto—boxing(自动打包):放入值得时候jdk自动将数值打包为对象才放入Map中
+ *  unboxing(解包):将对象转化为其他类型
  */
 public class TestMap {
   public static void main(String args[]) {
     Map m1 = new HashMap(); 
     Map m2 = new TreeMap();
     //m1.put("one",new Integer(1));
-    m1.put("one", 1);
+    m1.put("one", 1);  //Auto-boxing
     //m1.put("two",new Integer(2));
     m1.put("three", 3);
     //m1.put("three",new Integer(3));
@@ -30,7 +32,7 @@ public class TestMap {
     if(m1.containsKey("two")) {
       //int i = ((Integer)m1.get("two")).intValue();
     //使用get取数，强转
-      int i = (Integer)m1.get("two");
+      int i = (Integer)m1.get("two"); //unboxing
       System.out.println("i的值为："+i);
     }
     //将m1,m2放入m3的map中
