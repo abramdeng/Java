87163fa31f15079cf5340f7536c513f2d50bb916
ABRAM <ABRAM@ABRAM-Deng.SouthHuitong.csr.com>
2017-10-08 13:18:12 +0800

    计算单词出现的次数算法

--- Java/src/container/TestArgsWords.java
+++ Java/src/container/TestArgsWords.java
@@ -12,11 +12,19 @@ public class TestArgsWords {
 	  private static final int ONE = 1;
 	  public static void main(String args[]) {
 	    Map m = new HashMap();
+	    //
 	    for (int i = 0; i < args.length; i++) {
-	      //Integer freq = (Integer) m.get(args[i]);
+	    	
+	    	//拿m的key的value,如果value为空，则赋值freq为0。如果不为空则freq赋值为m的value
 	    	int freq = (Integer) m.get(args[i]) == null ? 0 : (Integer) m.get(args[i]);
-	      //m.put(args[i],(freq == null? ONE : new Integer(freq.intValue() + 1)));
-	      m.put(args[i],freq ==	0? ONE:freq +1);
+	    	//Integer freq = (Integer) m.get(args[i]);
+	    	
+	    	
+	    	//args[i]为输入的key,freq为0时赋值为1否则赋值freq+1(实现了计数的操作)
+	    	//例如aaa出现两次 第一次value=1 第二次freq不等于0，则value+1实现了单词的计数
+	        m.put(args[i],freq == 0? ONE:freq +1);
+	        //m.put(args[i],(freq == null? ONE : new Integer(freq.intValue() + 1)));
+	        
 	    }
 	    System.out.println
 	        (m.size() + " distinct words detected:");
