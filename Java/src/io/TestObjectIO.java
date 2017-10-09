5d1e32e6db7a06b4e2fe7ddb324e8fbb6800c085
ABRAM <ABRAM@ABRAM-Deng.SouthHuitong.csr.com>
2017-10-09 23:40:15 +0800

    ObjectStream示例（读写文件流存入对象）

---
+++ Java/src/io/TestObjectIO.java
@@ -0,0 +1,41 @@
+package io;
+
+import java.io.*;
+
+/***
+ * 
+ * @author ABRAM 2017.10.08
+ * 使用ObjectStream读写文件并写入到序列化类中然后进行操作
+ * transient值在序列化的时候无用
+ *
+ */
+public class TestObjectIO {
+	public static void main(String args[]) throws Exception {
+		T t = new T();
+		t.k = 8;
+		//输出流
+		FileOutputStream fos = new FileOutputStream("C:\\Users\\ABRAM\\Desktop\\1.txt");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		//写入流到对象t
+		oos.writeObject(t);
+		oos.flush();
+		oos.close();
+		//输入流
+		FileInputStream fis = new FileInputStream("C:\\Users\\ABRAM\\Desktop\\1.txt");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		//文件读出来为Object,强转为T
+		T tReaded = (T)ois.readObject();
+		System.out.println(tReaded.i + " " + tReaded.j + " " + tReaded.d + " " + tReaded.k);
+		
+	}
+}
+
+//将类实现序列化
+class T 
+	implements Serializable
+{
+	int i = 10;
+	int j = 9;
+	double d = 2.3;
+	transient int k = 15;
+}
